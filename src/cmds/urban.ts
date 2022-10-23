0e6150b6703a01a5e2621248485f9d4ae6405104
Meow <50887230+itzTheMeow@users.noreply.github.com>
2022-10-23 00:54:12 -0400

    add urban

--- src/cmds/urban.ts
+++ src/cmds/urban.ts
@@ -1,4 +1,5 @@
-import { define } from "urban-dictionary";
+import { getMarkdownTimestamp, MarkdownTimestampTypes } from "revolt-toolset";
+import define from "urban-eng-dictionary";
 import Command from "../Command";
 import config from "../config";
 
@@ -9,10 +10,15 @@ export default new Command(
   },
   async (bot, message, args) => {
     const searchTerm = args.asString();
-
     if (!searchTerm) return message.reply("Specify something to define!");
 
-    const defs = await define(searchTerm);
+    const defs: string[] = await (async () => {
+      try {
+        return await define(searchTerm);
+      } catch {
+        return [];
+      }
+    })();
     if (!defs.length) return message.reply("No definitions found.");
 
     const def = defs[0];
@@ -20,10 +26,14 @@ export default new Command(
     message.reply({
       embeds: [
         {
-          description: `#### ${def.word}
-
-${def.definition}`,
-          colour: config.brandColor,
+          description: `#### ${searchTerm}
+${def}`,
+          /*`
+##### :thumbsup: ${def.thumbs_up.toLocaleString()} :thumbsdown: ${def.thumbs_down.toLocaleString()}
+##### by ${def.author} on &nbsp;${getMarkdownTimestamp(
+            new Date(def.written_on).getTime(),
+            MarkdownTimestampTypes["22 September 2022"]
+          )}`*/ colour: config.brandColor,
         },
       ],
     });
