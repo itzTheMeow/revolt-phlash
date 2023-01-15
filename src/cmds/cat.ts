43489ffa3df105772941587a3588aa1531509616
Meow <50887230+itzTheMeow@users.noreply.github.com>
2023-01-14 23:43:25 -0500

    cat

---
+++ src/cmds/cat.ts
@@ -0,0 +1,25 @@
+import axios from "axios";
+import { msToString, uploadAttachment } from "revolt-toolset";
+import Command from "../Command";
+
+export default new Command(
+  "cat",
+  {
+    description: "Get an AI-generated cat from thiscatdoesnotexist.com.",
+  },
+  async (bot, message, args) => {
+    const startTime = Date.now();
+
+    axios
+      .get("https://thiscatdoesnotexist.com", {
+        responseType: "arraybuffer",
+      })
+      .then(async (res) => {
+        const id = await uploadAttachment("cat.png", res.data, "attachments");
+        message.reply({
+          content: `###### Finished in ${msToString(Date.now() - startTime)}.`,
+          attachments: [id],
+        });
+      });
+  }
+);
