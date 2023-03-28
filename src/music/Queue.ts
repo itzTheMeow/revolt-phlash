57b7aa626864d0e74e26acd5a05a5f48ae63c224
Meow <me@itsmeow.cat>
2023-03-27 23:22:18 -0400

    Update Queue.ts

--- src/music/Queue.ts
+++ src/music/Queue.ts
@@ -14,7 +14,7 @@ import { Filters, QueueFilter } from "./filters";
 import ServerQueueManager from "./ServerManager";
 import { shuffle } from "./util";
 
-const ytdlp = YTDLP(process.cwd() + "/yt-dlp");
+const ytdlp = YTDLP("/usr/local/bin/yt-dlp");
 
 export enum TrackProvider {
   YOUTUBE,
